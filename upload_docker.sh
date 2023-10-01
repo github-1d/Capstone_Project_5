2598a752f4c91e6bfe1c27e0c2352ea6d8735edd
github-1d <53148449+github-1d@users.noreply.github.com>
2023-10-01 14:44:54 +0530

    added docker image build and push steps
    added docker image build and push steps

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="monika2019/monika-flask-app:blue"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+# docker login --username monika2019
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker image tag monika-flask-app:blue $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
